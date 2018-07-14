a0d5a3a823e7a51936132b6b217fcb8ea92df499
Gaelen <gaelenrhoads@gmail.com>
2018-07-14 17:40:58 -0400

    enums

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -61,6 +61,10 @@ class BlogsController < ApplicationController
     end
   end
 
+  def toggle_status
+    byebug
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_blog
--- db/schema.rb
+++ db/schema.rb
@@ -21,7 +21,7 @@ ActiveRecord::Schema.define(version: 20180515001457) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "slug"
-    t.integer "status"
+    t.integer "status", default: 0
     t.index ["slug"], name: "index_blogs_on_slug", unique: true
   end
 
