ae45d35a51b0b455072e37136979263e693c9350
Gaelen <gaelenrhoads@gmail.com>
2018-09-19 12:44:09 -0400

    added concern for placeholder module

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180908160051) do
+ActiveRecord::Schema.define(version: 20180919162253) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -54,6 +54,7 @@ ActiveRecord::Schema.define(version: 20180908160051) do
     t.integer "percent_utlizied"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "badge"
   end
 
   create_table "topics", force: :cascade do |t|
