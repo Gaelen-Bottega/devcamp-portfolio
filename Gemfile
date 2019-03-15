698c7d6a4a32e6e09a81cf8ec0e3297d90342a06
Gaelen <gaelenrhoads@gmail.com>
2019-03-15 19:08:16 -0400

    might break

--- Gemfile
+++ Gemfile
@@ -53,3 +53,8 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 gem 'friendly_id', '~> 5.1.0'
 gem 'devise', '~> 4.6'
 
+gem 'bootstrap', '~> 4.3.1'
+
+gem 'jquery-rails'
+
+gem 'gaelen_copyright', git: "https://github.com/Gaelen-Bottega/gaelen_copyright"
--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -49,9 +49,10 @@ class PortfoliosController < ApplicationController
   end
 
   def destroy 
+    
     #Destroy the record
     @portfolio_item.destroy
-    
+
     #Redirect 
     respond_to do |format|
       format.html { redirect_to portfolios_url, notice: 'Record was removed.' }
