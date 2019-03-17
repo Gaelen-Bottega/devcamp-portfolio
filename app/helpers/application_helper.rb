9718979bb2275c94162d07c7d57a7c33aae05164
Gaelen <gaelenrhoads@gmail.com>
2019-03-17 15:46:03 -0400

    ruby gem refacter

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,4 +15,8 @@ module ApplicationHelper
       content_tag(:p, greeting, class: "source-greeting")
     end
   end
+
+  def copyright_generator
+    GaelenCopyright::Renderer.copyright 'Gaelen Rhoads', 'All Rights Reserved'
+  end
 end
